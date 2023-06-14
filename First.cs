3451b7aa85b4697d43a111f6b966b16e9e513c86
SahanaGowda12 <sahanamr121@gmail.com>
2023-06-14 13:14:54 +0530

    abc

--- First.cs
+++ First.cs
@@ -8,5 +8,17 @@ namespace Dev1
 {
     internal class First
     {
+        public string work()
+        {
+            Console.WriteLine("abcd");
+            Console.WriteLine("Sahana");
+            Console.WriteLine("welcome");
+            Console.WriteLine("Hi");
+           
+
+
+
+            return "hello";
+        }
     }
 }
